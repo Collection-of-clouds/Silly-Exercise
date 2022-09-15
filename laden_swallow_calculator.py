8b6b5296a871c4d8cbdfecd129517090883f67d5
Khalid Farah <56063705+KhalidAFarah@users.noreply.github.com>
2022-09-15 13:57:56 +0200

    Calculator now round up rather than show decimals

--- laden_swallow_calculator.py
+++ laden_swallow_calculator.py
@@ -1,6 +1,7 @@
 # https://www.youtube.com/watch?v=liIlW-ovx0Y
+import math
 
 def calc(coconut=454, swallow_capacity=20):
-  print(coconut / swallow_capacity)
+  print(math.ceil(coconut / swallow_capacity))
   
 calc()
